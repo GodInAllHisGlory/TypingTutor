15c6526e69d516c4110e92c1a045d2876f53639b
Nathan <nathanjohnhaight@gmail.com>
2025-11-03 00:26:53 -0700

    Adding back the fat

---
+++ src/Keyboard.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './Keyboard.css'
+
+function Keyboard() {
+
+    return(
+        <>
+            <div id="keyboard">
+
+            </div>
+        </>
+    )
+}
+
+export default Keyboard
