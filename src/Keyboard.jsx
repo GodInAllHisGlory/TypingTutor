5a0f1ebfcf950ddecb5dbf429bac96f29b5b2f70
Nathan <nathanjohnhaight@gmail.com>
2025-11-10 12:28:22 -0700

    Adds they keys you pushed into an array then remves them once relased

--- src/Keyboard.jsx
+++ src/Keyboard.jsx
@@ -12,24 +12,42 @@ const shiftRows = [["~","!","@","#","$","%","^","&","*","(",")","_","+"],
 ["Shift","Z","X","C","V","B","N","M","<",">","?","Shift"]];
 
 function Keyboard() {
-    useEffect(() => {function keydown(e) {
-    if (e.repeat) return;
-    console.log(e.key + " was pressed!");
-    
-}
+    const [pushedKeys, setKeys] = useState([]);
+    const [currentKeys, setRows] = useState(keyRows);
 
-    function keyup(e) {
-        console.log(e.key + " was released!");
-    }
-
+    useEffect(() => {
     window.addEventListener("keydown", keydown);
     window.addEventListener("keyup", keyup);
-    }, []); //Loads the event listens onto the window DOM element on the first render 
+    }, []); //Loads the event listeners into the window DOM element on the first render 
+    
+    function keydown(e) {
+        if (e.repeat) return;
+            const key = e.key;
+            console.log(key + " was pressed!");
+            if (key == "Shift"){
+                    setRows(shiftRows)
+                }
+                const newKeys = pushedKeys;
+                newKeys.push(key);
+                setKeys(newKeys);
+        }
+    
+    function keyup(e) {
+        const key = e.key
+        console.log(key + " was released!");
+        if (key == "Shift"){
+            setRows(keyRows)
+        }
+        const removeKey = pushedKeys;
+        removeKey.splice(removeKey.indexOf(key), 1);
+        setKeys(removeKey);
+        console.log(pushedKeys);
+    }
 
     let rowKey = 0
     return(
         <div id="keyboard">
-            {keyRows.map((row) => {
+            {currentKeys.map((row) => {
                 let symbolKey = 0
                 return(
                     <div className='row' key={`r${rowKey++}$`}>
