564c9b08c31b08cf2f117b886026984bdf1c4b01
Nathan <nathanjohnhaight@gmail.com>
2025-11-10 09:42:34 -0700

    Looks better now

--- src/Keyboard.jsx
+++ src/Keyboard.jsx
@@ -4,17 +4,18 @@ import './Keyboard.css';
 const keyRows = [["`","1","2","3","4","5","6","7","8","9","0","-","="],
 ["q","w","e","r","t","y","u","i","o","p","[","]","\\ "],
 ["a","s","d","f","g","h","j","k","l",";","'"],
-["LShift","z","x","c","v","b","n","m",",",".","/","RShift"]];
+["Shift","z","x","c","v","b","n","m",",",".","/","Shift"]];
 
 const shiftRows = [["~","!","@","#","$","%","^","&","*","(",")","_","+"],
 ["Q","W","E","R","T","Y","U","I","O","P","{","}","|"],
 ["A","S","D","F","G","H","J","K","L",";","'"],
-["LShift","Z","X","C","V","B","N","M","<",">","?","RShift"]];
+["Shift","Z","X","C","V","B","N","M","<",">","?","Shift"]];
 
 function Keyboard() {
     useEffect(() => {function keydown(e) {
     if (e.repeat) return;
     console.log(e.key + " was pressed!");
+    
 }
 
     function keyup(e) {
@@ -32,8 +33,13 @@ function Keyboard() {
                 let symbolKey = 0
                 return(
                     <div className='row' key={`r${rowKey++}$`}>
-                        {row.map(key =>{
-                            return <div className='key' key={`r${rowKey++}$k${symbolKey++}$`}>{key}</div> //Combine symbolKey and rowKey to make a unique key for each element on the keyboard
+                        {row.map(key => {
+                            let className = "key";
+
+                            if (key === "Shift"){
+                                className += " shift"
+                            }
+                            return (<div className={className} key={`r${rowKey++}$k${symbolKey++}$`}>{key}</div>) //Combine symbolKey and rowKey to make a unique key for each element on the keyboard
                         })}
                     </div>
                 )
