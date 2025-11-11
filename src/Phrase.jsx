6f82d5a8e7516c4d60b18b11cceadc86634cfbd0
Nathan <nathanjohnhaight@gmail.com>
2025-11-11 00:25:13 -0700

    The phrase gets shown and teh pointer works just need to update the pointer postion

--- src/Phrase.jsx
+++ src/Phrase.jsx
@@ -1,9 +1,20 @@
+import { useState } from 'react';
 import './Phrase.css'
 
 const phrases = ["The quick brown fox jumps over the nothing. This isn't a classic pangram."];
 
 export function Phrase() {
+    const [pointerPos, setPointerPos] = useState(0);
+    const currentPharse = phrases[0];
+    const typed = currentPharse.slice(0, pointerPos); //Gets all the chars before the pointer
+    const pointer = currentPharse[pointerPos];
+    const untyped = currentPharse.slice(pointerPos + 1); //Gets all the chars after the pointer
+
     return(
-        <span id="phrase">{phrases[0]}</span>
+        <span id="phrase">
+            <span id="typed">{typed}</span>
+            <span id="pointer">{pointer}</span>
+            {untyped}
+            </span>
     )
 }
