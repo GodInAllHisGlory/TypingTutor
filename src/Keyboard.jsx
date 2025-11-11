6bde76b48de2611a36be2628a6e9c6f61131966d
Nathan <nathanjohnhaight@gmail.com>
2025-11-10 23:38:55 -0700

    Added space bar

--- src/Keyboard.jsx
+++ src/Keyboard.jsx
@@ -4,12 +4,14 @@ import './Keyboard.css';
 const keyRows = [["`","1","2","3","4","5","6","7","8","9","0","-","="],
 ["q","w","e","r","t","y","u","i","o","p","[","]","\\"],
 ["a","s","d","f","g","h","j","k","l",";","'"],
-["Shift","z","x","c","v","b","n","m",",",".","/","Shift"]];
+["Shift","z","x","c","v","b","n","m",",",".","/","Shift"],
+[" "]];
 
 const shiftRows = [["~","!","@","#","$","%","^","&","*","(",")","_","+"],
 ["Q","W","E","R","T","Y","U","I","O","P","{","}","|"],
 ["A","S","D","F","G","H","J","K","L",";","'"],
-["Shift","Z","X","C","V","B","N","M","<",">","?","Shift"]];
+["Shift","Z","X","C","V","B","N","M","<",">","?","Shift"],
+[" "]];
 
 function Keyboard() {
     const [pushedKeys, setPushedKeys] = useState([]);
@@ -64,9 +66,11 @@ function Keyboard() {
 
                             if (key == "Shift"){
                                 className += " shift"
+                            }else if (key == " "){
+                                className += " space-bar"
                             }
 
-                            if(pushedKeys.indexOf(key) != -1){
+                            if (pushedKeys.indexOf(key) !== -1){
                                 className += " pushed"
                             }
 
